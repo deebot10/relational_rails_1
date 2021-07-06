a588592593c280c8b91fcc28cda90c4470be35e6
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-07-06 19:47:06 -0400

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/', to: 'welcome#index'
   #B
   #State routes
   get '/states', to: 'states#index'
-  get '/', to: 'welcome#index'
   get '/states/:id', to: 'states#show'
-
+  get '/parks', to: 'parks#index'
+  # get '/parks/:id', to: 'parks#show'
 
 
 
