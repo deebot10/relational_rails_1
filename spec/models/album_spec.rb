8f8a6b2f7fd449f321a4c951e791d236281e090b
deebot10 <dee1jhill@gmail.com>
2021-07-05 20:02:34 -0600

    Passed test for relationships

--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Album do
   describe 'relationships' do
-    it {should belong_to(:artist)}    
+    it {should belong_to(:artist) }    
   end
 
   describe 'instance methods'    
