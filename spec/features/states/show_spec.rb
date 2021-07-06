db9703685f42665cb6fe59d11ab3cf984e3016db
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-07-06 18:27:32 -0400

    Update feature test to check for page content

---
+++ spec/features/states/show_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe 'State' do
+  describe 'visit state index' do
+    before(:each) do
+      @state_1 = State.create!(name: 'Florida', region: 'Southeast', military_discount: true)
+      visit "/states/#{@state_1.id}"
+    end
+
+    it 'has the ability to read state content' do
+      expect(page).to have_content(@state_1.name)
+      expect(page).to have_content(@state_1.region)
+      expect(page).to have_content('has a military discount')
+      expect(page).to_not have_content('does not have a military discount')
+      save_and_open_page
+    end
+  end
+end
