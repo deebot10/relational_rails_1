fbcd783eb191060689a6dfece1d88361279f91f9
deebot10 <dee1jhill@gmail.com>
2021-07-07 17:51:54 -0600

    Create method for artist album count

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -10,6 +10,10 @@ RSpec.describe Artist do
     @artist_2 = Artist.create!(name: 'Noname', age: 29, genre: 'Rap', currently_touring: true)
     @artist_3 = Artist.create!(name: 'Earl Sweatshirt', age: 27, genre: 'Rap', currently_touring: false)
     @artist_4 = Artist.create!(name: 'Frank Ocean', age: 33, genre: 'R&B', currently_touring: false)
+
+    @album_1 = @artist_1.albums.create!(name: 'Awaken My Love', number_of_songs: 11, nominated: true)
+    @album_2 = @artist_1.albums.create!(name: 'Because the Internet', number_of_songs: 19, nominated: false)
+    @album_3 = @artist_1.albums.create!(name: 'Camp', number_of_songs: 13, nominated: false)
   end
 
   describe 'class methods' do
@@ -19,6 +23,12 @@ RSpec.describe Artist do
       expect(Artist.order_created).to eq([@artist_4, @artist_3, @artist_2, @artist_1])
     end    
   end
-  describe 'instance methods'     
+
+  describe 'instance methods'  
+    it 'counts the number of artist albums' do
+
+      expect(@artist_1.album_count).to eq(3)
+    end   
+    
   describe 'validations'
 end
