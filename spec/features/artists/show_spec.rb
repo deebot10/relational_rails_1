704a6d0d4459a4dec5b6e08d3bbd065c59979ac8
deebot10 <dee1jhill@gmail.com>
2021-07-11 16:48:28 -0600

    Wrote test for creating an Artist-Albums

--- spec/features/artists/show_spec.rb
+++ spec/features/artists/show_spec.rb
@@ -58,17 +58,6 @@ RSpec.describe 'Artist Show Page' do
   end 
 
   # User Story 12
-
-    # As a visitor
-    # When I visit a parent show page
-    # Then I see a link to update the parent "Update Parent"
-    # When I click the link "Update Parent"
-    # Then I am taken to '/parents/:id/edit' where I  see a form to edit the parent's attributes:
-    # When I fill out the form with updated information
-    # And I click the button to submit the form
-    # Then a `PATCH` request is sent to '/parents/:id',
-    # the parent's info is updated,
-    # and I am redirected to the Parent's Show page where I see the parent's updated info
   it 'can update and artist and the attributes' do
       
     click_button 'Edit Artist' 
--- spec/features/hits/index_spec.rb
+++ spec/features/hits/index_spec.rb
@@ -39,8 +39,6 @@ RSpec.describe 'Hits Index' do
     click_link 'Artists'
     expect(current_path).to eq('/artists')
   end
-  
-  [ ] done
 
     # User Story 13, Parent Child Creation (x2)
 
@@ -54,6 +52,20 @@ RSpec.describe 'Hits Index' do
     # Then a `POST` request is sent to '/parents/:parent_id/child_table_name',
     # a new child object/row is created for that parent,
     # and I am redirected to the Parent Childs Index page where I can see the new child listed
-  xit '' do
+  it 'can Create a new album for a Artist' do
+
+    click_button 'Create Album'
+
+    expect(current_path).to eq("/artists/#{@artist.id}/albums/new")
+
+    fill_in 'Name', with: '3.15.20'
+    fill_in 'Number of Tracks', with: 12 
+    fill_in 'Nominated', with: 'false'
+
+    click_button 'Add Album'
+
+    expect(current_path).to eq("/artists/#{artist.id}/albums")
+
+    expect(page).to have_content('3.15.20')
   end
 end
