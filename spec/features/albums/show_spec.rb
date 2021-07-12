3c11574dd686409b0479488465fd46de313a4742
deebot10 <dee1jhill@gmail.com>
2021-07-11 18:55:25 -0600

    Create test for edit album

--- spec/features/albums/show_spec.rb
+++ spec/features/albums/show_spec.rb
@@ -26,4 +26,31 @@ RSpec.describe 'Album Show' do
     click_link 'Artists'
     expect(current_path).to eq('/artists')
   end
+
+  # User Story 14
+    # As a visitor
+    # When I visit a Child Show page
+    # Then I see a link to update that Child "Update Child"
+    # When I click the link
+    # I am taken to '/child_table_name/:id/edit' where I see a form to edit the child's attributes:
+    # When I click the button to submit the form "Update Child"
+    # Then a `PATCH` request is sent to '/child_table_name/:id',
+    # the child's data is updated,
+    # and I am redirected to the Child Show page where I see the Child's updated information
+  it 'Can update an Album' do
+
+    click_button 'Edit Album'
+
+    expect(current_path).to eq("/albums/#{@album_1.id}/edit")
+
+    fill_in 'Name', with: '3.15.20'
+    fill_in 'Number of songs', with: 12 
+    fill_in 'Nominated', with: 'false'
+
+    click_button 'Edit Album'
+    
+    expect(current_path).to eq("/albums/#{album_1.id}")
+
+    expect(page).to have_content('3.15.20')
+  end
 end
--- spec/features/hits/index_spec.rb
+++ spec/features/hits/index_spec.rb
@@ -40,18 +40,7 @@ RSpec.describe 'Hits Index' do
     expect(current_path).to eq('/artists')
   end
 
-    # User Story 13, Parent Child Creation (x2)
-
-    # As a visitor
-    # When I visit a Parent Childs Index page
-    # Then I see a link to add a new adoptable child for that parent "Create Child"
-    # When I click the link
-    # I am taken to '/parents/:parent_id/child_table_name/new' where I see a form to add a new adoptable child
-    # When I fill in the form with the child's attributes:
-    # And I click the button "Create Child"
-    # Then a `POST` request is sent to '/parents/:parent_id/child_table_name',
-    # a new child object/row is created for that parent,
-    # and I am redirected to the Parent Childs Index page where I can see the new child listed
+  # User Story 13
   it 'can Create a new album for a Artist' do
 
     click_button 'Add Album'
@@ -67,6 +56,5 @@ RSpec.describe 'Hits Index' do
     expect(current_path).to eq("/artists/#{@artist.id}/albums")
 
     expect(page).to have_content('3.15.20')
-    save_and_open_page
   end
 end
