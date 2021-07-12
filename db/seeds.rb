e02665df8ea08fd44dfdb38c89301a62ff602fbb
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-07-12 10:46:49 -0400

    Update seeds file with reference data

--- db/seeds.rb
+++ db/seeds.rb
@@ -37,22 +37,26 @@ Album.destroy_all
 
 
 
-@state_1 = State.create!(name: 'Florida', region: 'Southeast', military_discount: true, green_rank: 29)
+@state_1 = State.create!(name: 'Florida', region: 'Southeast', military_discount: true, green_rank: 42)
 @park_1 = @state_1.parks.create!(name: 'Ginny Springs', camping_allowed: true, kayaking_available: true, park_rating: 4.6)
 @park_2 = @state_1.parks.create!(name: 'Collier-Seminole State Park', camping_allowed: true, kayaking_available: true, park_rating: 4.4)
 
-@state_2 = State.create!(name: 'Maine', region: 'Northeast', military_discount: true, green_rank: 3)
+@state_2 = State.create!(name: 'Maine', region: 'Northeast', military_discount: true, green_rank: 12)
 @park_3 = @state_2.parks.create!(name: 'Sebago Lake State Park', camping_allowed: true, kayaking_available: true, park_rating: 4.7)
 @park_4 = @state_2.parks.create!(name: "Wolfe's Neck Woods State Park", camping_allowed: false, kayaking_available: false, park_rating: 4.5)
 
-@state_3 = State.create!(name: 'Hawaii', region: 'Southwest', military_discount: false, green_rank: 2)
+@state_3 = State.create!(name: 'Hawaii', region: 'West', military_discount: false, green_rank: 8)
 @park_5 = @state_3.parks.create!(name: 'Makena', camping_allowed: false, kayaking_available: true, park_rating: 4.8)
 @park_6 = @state_3.parks.create!(name: "Wai'anapanapa State Park", camping_allowed: false, kayaking_available: false, park_rating: 4.7)
 
-@state_4 = State.create!(name: 'California', region: 'Southwest', military_discount: true, green_rank: 46)
+@state_4 = State.create!(name: 'California', region: 'West', military_discount: true, green_rank: 5)
 @park_7 = @state_4.parks.create!(name: 'Folsom Lake State Park', camping_allowed: true, kayaking_available: true, park_rating: 4.6)
 @park_8 = @state_4.parks.create!(name: "McArthur-Burney Falls State Park", camping_allowed: true, kayaking_available: true, park_rating: 4.8)
 
-@state_5 = State.create!(name: 'Mississippi', region: 'Southeast', military_discount: false, green_rank: 40)
+@state_5 = State.create!(name: 'Mississippi', region: 'Southeast', military_discount: false, green_rank: 48)
 @park_9 = @state_5.parks.create!(name: 'Shepard State Park', camping_allowed: true, kayaking_available: true, park_rating: 4.3)
 @park_10 = @state_5.parks.create!(name: "Clarkco State Park", camping_allowed: true, kayaking_available: true, park_rating: 4.6)
+
+@state_6 = State.create!(name: 'Vermont', region: 'Northeast', military_discount: false, green_rank: 1)
+@park_9 = @state_5.parks.create!(name: 'Grand Isle State Park', camping_allowed: true, kayaking_available: true, park_rating: 4.8)
+@park_10 = @state_5.parks.create!(name: "Underhill State Park", camping_allowed: true, kayaking_available: false, park_rating: 4.9)
