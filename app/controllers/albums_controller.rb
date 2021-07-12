cf51af3aeb927d60c56eb28c2934bf5077d1dcfb
deebot10 <dee1jhill@gmail.com>
2021-07-11 21:36:01 -0600

    Complete user story 20

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -17,6 +17,12 @@ class AlbumsController < ApplicationController
     redirect_to "/albums/#{album.id}"
   end
 
+  def destroy
+    album = Album.find(params[:id])
+    album.destroy 
+    redirect_to '/albums'
+  end
+
   private
   def album_params
     params.permit(:name, :number_of_songs, :nominated)
