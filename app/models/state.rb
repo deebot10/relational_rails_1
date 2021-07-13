d77661ab9310d1c1e5b4d2e1492df322aebe9403
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-07-13 13:34:35 -0400

    Polish

--- app/models/state.rb
+++ app/models/state.rb
@@ -1,7 +1,5 @@
 class State < ApplicationRecord
   has_many :parks, dependent: :destroy
-   # -> { order(created_at: :desc) },
-  # scope :most_recent, -> (50) { order("created_at desc").limit(50) }
 
   def parks_count
     parks.count
