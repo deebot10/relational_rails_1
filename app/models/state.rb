2b7cae14b6d9c847d9490dc80546738bb6b82e34
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-07-14 09:31:52 -0400

    Refactored quant method to LEFT JOIN, accounts for 0 children, tested

--- app/models/state.rb
+++ app/models/state.rb
@@ -6,7 +6,8 @@ class State < ApplicationRecord
   end
 
   def self.quant
-    joins(:parks).group("states.id").order("COUNT(states.id) DESC")
+    # joins(:parks).group("states.id").order(Arel.sql("COUNT(states.id) DESC"))
+    joins("LEFT JOIN parks ON parks.state_id = states.id").group("states.id").order(Arel.sql("COUNT(states.id) DESC"))
   end
 
 end
--- spec/features/states/index_spec.rb
+++ spec/features/states/index_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe 'State' do
       @state_1 = State.create!(name: 'Florida', region: 'Southeast', military_discount: true, green_rank: 29)
       @state_2 = State.create!(name: 'Maine', region: 'Northeast', military_discount: true, green_rank: 3)
       @state_3 = State.create!(name: 'Washington', region: 'Northwest', military_discount: false, green_rank: 12)
+      @state_4 = State.create!(name: 'Oregon', region: 'Northwest', military_discount: false, green_rank: 19)
+
       visit '/states'
     end
 
@@ -38,7 +40,7 @@ RSpec.describe 'State' do
     it 'has a link to the list of parks' do
       expect(page).to have_link('Parks')
 
-      click_link 'Park'
+      click_link 'Parks'
 
       expect(current_path).to eq('/parks')
     end
@@ -58,12 +60,38 @@ RSpec.describe 'State' do
     end
 
     it 'can delete the artist from the index page' do
-      visit '/states'
-
       click_button "Delete #{@state_1.name}"
 
       expect(current_path).to eq('/states')
       expect(page).to_not have_content('Florida')
+    end
+
+    it 'has a link to order the states by park quantity' do
+      expect(page).to have_link("Sort States by Quantity of Parks")
+    end
+
+    it 'can actually reorder the states by park quantity' do
+      @park_1 = @state_1.parks.create!(name: 'Collier-Seminole State Park', camping_allowed: true, kayaking_available: true, park_rating: 4.4)
+      @park_2 = @state_1.parks.create!(name: 'Ginny Springs', camping_allowed: true, kayaking_available: true, park_rating: 4.6)
+      @park_3 = @state_1.parks.create!(name: 'Biscayne Bay', camping_allowed: false, kayaking_available: true, park_rating: 4.8)
+      @park_4 = @state_2.parks.create!(name: 'Sebago Lake State Park', camping_allowed: true, kayaking_available: true, park_rating: 4.7)
+      @park_5 = @state_2.parks.create!(name: "Wolfe's Neck Woods State Park", camping_allowed: false, kayaking_available: false, park_rating: 4.5)
+      @park_6 = @state_3.parks.create!(name: 'Crater Lake', camping_allowed: true, kayaking_available: true, park_rating: 4.6)
+
+      this = 'Washington'
+      that = 'Maine'
+      the_other = 'Florida'
+      no_parks = 'Oregon'
+
+      expect(no_parks).to appear_before(this)
+      expect(this).to appear_before(that)
+      expect(that).to appear_before(the_other)
+      click_link "Sort States by Quantity of Parks"
+
+      expect(the_other).to appear_before(that)
+      expect(the_other).to appear_before(this)
+      expect(that).to appear_before(this)
+      expect(this).to appear_before(no_parks)
     end
   end
 end
