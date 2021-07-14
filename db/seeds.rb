0e949c8aa41b5cf4e2d48f7635b777b224cb075e
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-07-14 14:00:07 -0400

    Update seeds to give different quantites to states to prove sorting

--- db/seeds.rb
+++ db/seeds.rb
@@ -56,7 +56,10 @@ Album.destroy_all
 @state_5 = State.create!(name: 'Mississippi', region: 'Southeast', military_discount: false, green_rank: 48)
 @park_9 = @state_5.parks.create!(name: 'Shepard State Park', camping_allowed: true, kayaking_available: true, park_rating: 4.3)
 @park_10 = @state_5.parks.create!(name: "Clarkco State Park", camping_allowed: true, kayaking_available: true, park_rating: 4.6)
+@park_11 = @state_6.parks.create!(name: "Franklin State Park", camping_allowed: false, kayaking_available: false, park_rating: 4.4)
 
 @state_6 = State.create!(name: 'Vermont', region: 'Northeast', military_discount: false, green_rank: 1)
-@park_9 = @state_6.parks.create!(name: 'Grand Isle State Park', camping_allowed: true, kayaking_available: true, park_rating: 4.8)
-@park_10 = @state_6.parks.create!(name: "Underhill State Park", camping_allowed: true, kayaking_available: false, park_rating: 4.9)
+@park_12 = @state_6.parks.create!(name: 'Grand Isle State Park', camping_allowed: true, kayaking_available: true, park_rating: 4.8)
+@park_13 = @state_6.parks.create!(name: "Underhill State Park", camping_allowed: true, kayaking_available: false, park_rating: 4.9)
+@park_14 = @state_6.parks.create!(name: "Underhill State Park", camping_allowed: true, kayaking_available: false, park_rating: 4.9)
+@park_15 = @state_6.parks.create!(name: "Half Moon State Park", camping_allowed: true, kayaking_available: true, park_rating: 4.7)
